8836ac39b0e9f0b6212377b1bbc0a6df021ec840
Roman <roman@burningswell.com>
2012-03-13 21:06:09 +0100

    Get ready for Leiningen 2.

--- project.clj
+++ project.clj
@@ -1,15 +1,18 @@
-(defproject database-clj "0.0.1-SNAPSHOT"
+(defproject database-clj "0.0.2-SNAPSHOT"
   :description "Clojure Database Kung-Foo"
+  :min-lein-version "2.0.0"
   :dependencies [[c3p0/c3p0 "0.9.1.2"]
-                 [clj-time "0.3.6-SNAPSHOT"]
-                 [geo-clj "0.0.1-SNAPSHOT"]
-                 [inflections "0.6.5-SNAPSHOT"]
+                 [clj-time "0.3.7"]
+                 [geo-clj "0.0.3-SNAPSHOT"]
+                 [inflections "0.7.0-SNAPSHOT"]
                  [korma "0.3.0-beta5"]
-                 [migrate "0.0.6-SNAPSHOT"]
+                 [migrate "0.0.7-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.0.7"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.postgis/postgis-jdbc "1.3.3"]
                  [org.slf4j/slf4j-log4j12 "1.5.6"]
                  [postgresql/postgresql "9.1-901.jdbc4"]]
-  :dev-dependencies [[validation-clj "0.2.1-SNAPSHOT"]])
+  :profiles {:dev {:dependencies [[validation-clj "0.2.1-SNAPSHOT"]]
+                   :resource-paths ["test-resources"]}}
+  :plugins [[migrate "0.0.7-SNAPSHOT"]])
