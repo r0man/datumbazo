66f8c4d1a85687a3b85f67431ff79b9d79a860d3
Roman <roman@burningswell.com>
2012-04-16 20:07:51 +0200

    Switched to Clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Clojure Database Kung-Foo"
   :min-lein-version "2.0.0"
   :dependencies [[c3p0/c3p0 "0.9.1.2"]
-                 [clj-time "0.3.7"]
+                 [clj-time "0.4.0"]
                  [geo-clj "0.0.3-SNAPSHOT"]
                  [inflections "0.7.0-SNAPSHOT"]
                  [korma "0.3.0-beta6"]
                  [migrate "0.0.7-SNAPSHOT"]
-                 [org.clojure/clojure "1.3.0"]
-                 [org.clojure/java.jdbc "0.0.7"]
+                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/java.jdbc "0.1.4"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.postgis/postgis-jdbc "1.3.3"]
                  [org.slf4j/slf4j-log4j12 "1.5.6"]
