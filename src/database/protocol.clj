e994499adb5e66a5e16544d06cfcbdf9b79bcb1f
Roman Scherer <roman.scherer@burningswell.com>
2012-08-14 23:02:14 +0200

    More keyword and identifier tests.

--- src/database/protocol.clj
+++ src/database/protocol.clj
@@ -2,20 +2,28 @@
   (:require [clojure.java.jdbc :as jdbc]))
 
 (defprotocol Nameable
+  (as-keyword [obj]
+    "Returns `obj` as a keyword.")
   (as-identifier [obj]
-    "Returns the database identifier of `obj`."))
+    "Returns `obj` as a database identifier"))
 
 (extend-type clojure.lang.Keyword
   Nameable
+  (as-keyword [k]
+    (jdbc/as-keyword k))
   (as-identifier [k]
     (jdbc/as-identifier k)))
 
 (extend-type clojure.lang.Symbol
   Nameable
+  (as-keyword [s]
+    (jdbc/as-keyword s))
   (as-identifier [s]
     (jdbc/as-identifier (keyword s))))
 
 (extend-type java.lang.String
   Nameable
+  (as-keyword [s]
+    (jdbc/as-keyword s))
   (as-identifier [s]
     (jdbc/as-identifier s)))
--- test/database/test/protocol.clj
+++ test/database/test/protocol.clj
@@ -1,17 +1,39 @@
 (ns database.test.protocol
-  (:refer-clojure :exclude [drop])
+  (:require [clojure.java.jdbc :as jdbc]
+            [inflections.core :refer [dasherize underscore]])
   (:use clojure.test
         database.connection
         database.protocol
         database.schema))
 
+(deftest test-as-keyword
+  (are [obj expected]
+    (is (= expected (as-keyword obj)))
+    :weather-models :weather-models
+    "weather-models" :weather-models
+    "weather_models" :weather_models)
+  (jdbc/with-naming-strategy {:keyword dasherize}
+    (are [obj expected]
+      (is (= expected (as-keyword obj)))
+      :weather-models :weather-models
+      "weather-models" :weather-models
+      "weather_models" :weather-models)))
+
 (deftest test-as-identifier
   (are [obj expected]
     (is (= expected (as-identifier obj)))
     :weather-models "weather-models"
-    "weather-models" "weather-models")
+    "weather-models" "weather-models"
+    "weather_models" "weather_models")
   (with-quoted-identifiers \"
     (are [obj expected]
       (is (= expected (as-identifier obj)))
       :weather-models "\"weather-models\""
-      "weather-models" "weather-models")))
+      "weather-models" "weather-models"
+      "weather_models" "weather_models"))
+  (jdbc/with-naming-strategy {:entity underscore}
+    (are [obj expected]
+      (is (= expected (as-identifier obj)))
+      :weather-models "weather_models"
+      "weather-models" "weather-models"
+      "weather_models" "weather_models")))
