0b019736967c565454af1e616e366406f7efe120
Roman Scherer <roman.scherer@burningswell.com>
2012-10-01 21:33:06 +0200

    Added deftable.

--- src/database/core.clj
+++ src/database/core.clj
@@ -1,5 +1,6 @@
 (ns database.core
-  (:require [clojure.java.jdbc :as jdbc]
+  (:require [clojure.algo.monads :refer [m-seq state-m with-monad]]
+            [clojure.java.jdbc :as jdbc]
             [environ.core :refer [env]]))
 
 (defmacro with-connection
@@ -14,5 +15,37 @@
          name#)
        ~@body)))
 
-(defn make-table [name]
-  {:table-name (keyword name)})
+(defn make-table [name & {:as options}]
+  (assoc options
+    :name (keyword name)))
+
+(defn make-column [name type & {:as options}]
+  (assoc options
+    :name (keyword name)))
+
+(defn column [table name type & options]
+  (let [column (apply make-column name type options)]
+    (-> (update-in table [:columns] #(concat %1 [(:name column)]))
+        (assoc-in [:column (:name column)] column))))
+
+(defn schema [table schema]
+  (assoc table :schema schema))
+
+(defn register-table [table]
+  table)
+
+(defmacro deftable
+  "Compose multiple fields into one field-like item."
+  [name doc & body]
+  `(def ~name
+     (register-table
+      (-> (make-table ~(keyword name) :doc ~doc)
+          ~@body))))
+
+;; (deftable continents)
+;; (continents :x 1)
+
+;; (deftable countries
+;;   (column :id :serial)
+;;   (column :continent-id :integer :references :continents/id)
+;;   (column :name))
--- test/database/core_test.clj
+++ test/database/core_test.clj
@@ -2,6 +2,41 @@
   (:use clojure.test
         database.core))
 
+(deftable models
+  "The weather models table."
+  (schema :weather)
+  (column :id :serial)
+  (column :name :text))
+
+(deftable variables
+  "The weather variables table."
+  (schema :weather)
+  (column :id :serial)
+  (column :name :text))
+
+(deftable models-variables
+  "The join table between weather models and variables."
+  (schema :weather)
+  (column :model-id :integer)
+  (column :variable-id :integer))
+
+(deftest test-make-table
+  (let [table (make-table :continents)]
+    (is (= :continents (:name table))))
+  (is (= (make-table :continents) (make-table "continents"))))
+
+(deftest test-deftable
+  (let [table models]
+    (is (= :weather (:schema table)))
+    (is (= :models (:name table)))
+    (is (= [:id :name] (:columns table)))
+    (is (= {:name {:name :name}
+            :id {:name :id}}
+           (:column table)))
+    (is (= "The weather models table." (:doc table)))))
+
+;; (clojure.pprint/pprint models)
+
 (deftest test-with-connection
   (are [spec]
     (with-connection spec
