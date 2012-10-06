c29b29807a7d69112859ffcc64a4e97e623fed65
Roman Scherer <roman.scherer@burningswell.com>
2012-10-06 22:28:36 +0200

    Added deftable tests.

--- src/database/connection.clj
+++ src/database/connection.clj
@@ -6,18 +6,20 @@
 (def ^:dynamic *naming-strategy*
   {:entity underscore :keyword dasherize})
 
+(defn resolve-connection
+  [db-spec]
+  (if (keyword? db-spec)
+    (or (env db-spec)
+        (throw (IllegalArgumentException. (format "Can't resolve connection spec via environ: %s" db-spec))))
+    db-spec))
+
 (defmacro with-connection
-  "Evaluates body in the context of a new connection to the `name`
-  database. The connection spec for `name` is looked up via environ."
+  "Evaluates body in the context of a connection to the database
+  `name`. The connection spec for `name` is looked up via environ."
   [name & body]
-  `(let [name# ~name]
-     (jdbc/with-naming-strategy *naming-strategy*
-       (jdbc/with-connection
-         (if (keyword? name#)
-           (or (env name#)
-               (throw (IllegalArgumentException. (format "Can't resolve connection spec via environ: %s" name#))))
-           name#)
-         ~@body))))
+  `(jdbc/with-naming-strategy *naming-strategy*
+     (jdbc/with-connection (resolve-connection ~name)
+       ~@body)))
 
 (defn wrap-connection
   "Returns a Ring handler with that has a connection to the `db-spec`."
--- test/database/test/core.clj
+++ test/database/test/core.clj
@@ -2,3 +2,37 @@
   (:require [clojure.java.jdbc :as jdbc])
   (:use database.core
         clojure.test))
+
+(deftable continents
+  "The continents database table."
+  (column :id :serial)
+  (column :name :text))
+
+(deftable countries
+  "The countries database table."
+  (column :id :serial)
+  (column :continent-id :integer :references :continents/id)
+  (column :name :text))
+
+(deftest test-continents-table
+  (is (= :continents (:name continents)))
+  (is (= [:id :name] (:columns continents)))
+  (let [column (:id (:column continents))]
+    (is (= :id (:name column)))
+    (is (= :serial (:type column))))
+  (let [column (:name (:column continents))]
+    (is (= :name (:name column)))
+    (is (= :text (:type column)))))
+
+(deftest test-countries-table
+  (is (= :countries (:name countries)))
+  (is (= [:id :continent-id :name] (:columns countries)))
+  (let [column (:id (:column countries))]
+    (is (= :id (:name column)))
+    (is (= :serial (:type column))))
+  (let [column (:continent-id (:column countries))]
+    (is (= :continent-id (:name column)))
+    (is (= :integer (:type column))))
+  (let [column (:name (:column countries))]
+    (is (= :name (:name column)))
+    (is (= :text (:type column)))))
