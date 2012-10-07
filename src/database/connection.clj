879238d69814768aa747c357aca329101c423480
Roman Scherer <roman.scherer@burningswell.com>
2012-10-07 13:23:27 +0200

    Added make-connection-pool.

--- src/database/connection.clj
+++ src/database/connection.clj
@@ -1,10 +1,20 @@
 (ns database.connection
+  (:import com.mchange.v2.c3p0.ComboPooledDataSource)
   (:require [clojure.java.jdbc :as jdbc]
+            [clojure.string :refer [blank?]]
+            [database.util :refer [parse-url parse-integer]]
             [environ.core :refer [env]]
             [inflections.core :refer [dasherize underscore]]))
 
+(def ^:dynamic *connection* nil)
+
 (def ^:dynamic *naming-strategy*
   {:entity underscore :keyword dasherize})
+
+(def ^:dynamic *pool-params*
+  {:max-pool-size 15
+   :max-idle-time (* 3 60 60)
+   :max-idle-time-excess-connections (* 30 60)})
 
 (defn resolve-connection
   [db-spec]
@@ -12,6 +22,21 @@
     (or (env db-spec)
         (throw (IllegalArgumentException. (format "Can't resolve connection spec via environ: %s" db-spec))))
     db-spec))
+
+(defn make-connection-pool
+  "Make a C3P0 database connection pool."
+  [url]
+  (if-let [db-spec (parse-url url)]
+    (let [params (merge *pool-params* (:params db-spec))]
+      {:datasource
+       (doto (ComboPooledDataSource.)
+         (.setDriverClass (:classname db-spec))
+         (.setJdbcUrl (str "jdbc:" (:subprotocol db-spec) ":" (:subname db-spec)))
+         (.setUser (:user db-spec))
+         (.setPassword (:password db-spec))
+         (.setMaxIdleTimeExcessConnections (parse-integer (:max-idle-time-excess-connections params)))
+         (.setMaxIdleTime (parse-integer (:max-idle-time params)))
+         (.setMaxPoolSize (parse-integer (:max-pool-size params))))})))
 
 (defmacro with-connection
   "Evaluates body in the context of a connection to the database
--- test/database/test/connection.clj
+++ test/database/test/connection.clj
@@ -1,7 +1,20 @@
 (ns database.test.connection
+  (:import com.mchange.v2.c3p0.ComboPooledDataSource)
   (:require [clojure.java.jdbc :as jdbc])
   (:use database.connection
         clojure.test))
+
+(deftest test-make-connection-pool
+  (let [pool (make-connection-pool "postgresql://localhost/test")]
+    (is (map? pool))
+    (let [datasource (:datasource pool)]
+      (is (instance? ComboPooledDataSource datasource))
+      (is (= "jdbc:postgresql://localhost/test" (.getJdbcUrl datasource)))
+      (is (= 15 (.getMaxPoolSize datasource)))
+      (is (= 10800 (.getMaxIdleTime datasource)))
+      (is (= 1800 (.getMaxIdleTimeExcessConnections datasource))))
+    (with-connection pool
+      (is (jdbc/connection)))))
 
 (deftest test-with-connection
   (with-connection :bs-database
