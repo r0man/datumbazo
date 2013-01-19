844b0b9f5c802558e6dcf29981308440a55286f9
Roman Scherer <roman.scherer@burningswell.com>
2013-01-19 19:21:04 +0100

    Added test-read-instant-date-time.

--- test/datumbazo/test/io.clj
+++ test/datumbazo/test/io.clj
@@ -1,6 +1,8 @@
 (ns datumbazo.test.io
-  (:import org.postgis.Point
-           org.postgis.PGgeometry)
+  (:import java.util.Date
+           org.postgis.Point
+           org.postgis.PGgeometry
+           org.joda.time.DateTime)
   (:require [clojure.java.jdbc :as jdbc])
   (:use datumbazo.io
         clojure.test))
@@ -15,3 +17,14 @@
        (is (= expected (pr-str s)))
        (Point. 1 2) "#wkt \"POINT(1 2)\""
        (PGgeometry. (Point. 1 2)) "#wkt \"POINT(1 2)\""))
+
+(deftest test-read-instant-date-time
+  (let [date (Date. 0)
+        time (DateTime. 0)]
+    (is (= "#inst \"1970-01-01T00:00:00.000-00:00\"" (pr-str date)))
+    (is (= "#inst \"1970-01-01T00:00:00.000-00:00\"" (pr-str time)))
+    (is (= date (read-string (pr-str date))))
+    (is (= date (read-string (pr-str time))))
+    (binding [*data-readers* {'inst read-instant-date-time}]
+      (is (= time (read-string (pr-str date))))
+      (is (= time (read-string (pr-str time)))))))
