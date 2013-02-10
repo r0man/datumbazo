69b618221e11ff1202e83d953f5c0f1066591d54
Roman Scherer <roman.scherer@burningswell.com>
2013-02-10 15:07:38 +0100

    Bumped versions.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datumbazo "0.2.0-SNAPSHOT"
+(defproject datumbazo "0.2.1-SNAPSHOT"
   :description "Clojure Database Kung-Foo"
   :url "https://github.com/r0man/datumbazo"
   :min-lein-version "2.0.0"
@@ -13,7 +13,7 @@
                  [org.postgis/postgis-jdbc "1.3.3"]
                  [postgresql/postgresql "9.1-901.jdbc4"]
                  [slingshot "0.10.3"]
-                 [sqlingvo "0.2.0-SNAPSHOT"]]
+                 [sqlingvo "0.2.1-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[validation-clj "0.5.1-SNAPSHOT"]
                                   [org.slf4j/slf4j-log4j12 "1.6.6"]
                                   [c3p0/c3p0 "0.9.1.2"]
