4e3482173f1c7757965925b104c1dcc0e760835e
Roman Scherer <roman.scherer@burningswell.com>
2013-03-09 19:00:51 +0100

    Release 0.3.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datumbazo "0.2.1-SNAPSHOT"
+(defproject datumbazo "0.3.0"
   :description "Clojure Database Kung-Foo"
   :url "https://github.com/r0man/datumbazo"
   :min-lein-version "2.0.0"
@@ -13,7 +13,7 @@
                  [org.cloudhoist/stevedore "0.7.3"]
                  [postgresql/postgresql "9.1-901.jdbc4"]
                  [slingshot "0.10.3"]
-                 [sqlingvo "0.2.1-SNAPSHOT"]]
+                 [sqlingvo "0.3.0"]]
   :profiles {:dev {:dependencies [[validation-clj "0.5.1-SNAPSHOT"]
                                   [org.slf4j/slf4j-log4j12 "1.6.6"]
                                   [c3p0/c3p0 "0.9.1.2"]
