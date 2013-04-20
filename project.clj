8a2fe67f59fb1c5d12000b8bc6a1b61a86784d89
Roman Scherer <roman.scherer@burningswell.com>
2013-04-20 17:07:50 +0200

    Use validation-clj 0.5.3-SNAPSHOT in dev dependencies.

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [postgresql/postgresql "9.1-901.jdbc4"]
                  [slingshot "0.10.3"]
                  [sqlingvo "0.4.1"]]
-  :profiles {:dev {:dependencies [[validation-clj "0.5.2-SNAPSHOT"]
+  :profiles {:dev {:dependencies [[validation-clj "0.5.3-SNAPSHOT"]
                                   [org.slf4j/slf4j-log4j12 "1.6.6"]
                                   [c3p0/c3p0 "0.9.1.2"]
                                   [com.jolbox/bonecp "0.7.1.RELEASE"]]
