1f919ebb3938795dc3215eb911a1004b5acd7aa2
Roman Scherer <roman.scherer@burningswell.com>
2013-05-04 15:02:28 +0200

    Switched to sqlingvo 0.4.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datumbazo "0.4.5"
+(defproject datumbazo "0.4.6-SNAPSHOT"
   :description "Clojure Database Kung-Foo"
   :url "https://github.com/r0man/datumbazo"
   :min-lein-version "2.0.0"
@@ -14,7 +14,7 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [postgresql/postgresql "9.1-901.jdbc4"]
                  [slingshot "0.10.3"]
-                 [sqlingvo "0.4.1"]]
+                 [sqlingvo "0.4.2"]]
   :profiles {:dev {:dependencies [[validation-clj "0.5.4"]
                                   [org.slf4j/slf4j-log4j12 "1.6.6"]
                                   [c3p0/c3p0 "0.9.1.2"]
