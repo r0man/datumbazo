b21ebfa2bc111a71765c9f06ac7982440ef2904f
Roman Scherer <roman.scherer@burningswell.com>
2013-08-15 19:39:49 +0200

    Release 0.5.0.

--- project.clj
+++ project.clj
@@ -1,22 +1,22 @@
-(defproject datumbazo "0.4.9-SNAPSHOT"
+(defproject datumbazo "0.5.0"
   :description "Clojure Database Kung-Foo"
   :url "https://github.com/r0man/datumbazo"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-time "0.5.0"]
-                 [com.palletops/stevedore "0.8.0-beta.2"]
+  :dependencies [[clj-time "0.5.1"]
+                 [com.palletops/stevedore "0.8.0-beta.4"]
                  [environ "0.4.0"]
-                 [geo-clj "0.2.5"]
+                 [geo-clj "0.2.8"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.3.0-alpha4"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [postgresql/postgresql "9.1-901.jdbc4"]
-                 [mysql/mysql-connector-java "5.1.25"]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]
+                 [mysql/mysql-connector-java "5.1.26"]
                  [slingshot "0.10.3"]
-                 [sqlingvo "0.5.0-SNAPSHOT"]]
+                 [sqlingvo "0.5.0"]]
   :profiles {:dev {:dependencies [[validation-clj "0.5.4"]
-                                  [org.slf4j/slf4j-log4j12 "1.6.6"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.5"]
                                   [c3p0/c3p0 "0.9.1.2"]
                                   [com.jolbox/bonecp "0.7.1.RELEASE"]]
                    :resource-paths ["test-resources"]
