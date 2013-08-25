147992f0a8df73f1dd961b90f93415f580a4001e
Roman Scherer <roman.scherer@burningswell.com>
2013-08-25 15:59:17 +0200

    Try scripts from activerecord-postgis-adapter.

--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-dropdb database_test
-./bin/createdb database_test
-lein test
---
+++ travis/before_script.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+psql -c "CREATE DATABASE datumbazo;" -U postgres
+psql -c "CREATE USER tiger SUPERUSER UNENCRYPTED PASSWORD 'scotch';" -U postgres
+psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-postgis-extension.sql
+psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-citext-extension.sql
+psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-continents-table.sql
+psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-countries-table.sql
+psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-twitter-schema.sql
+psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-twitter-users-table.sql
+psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-twitter-tweets-table.sql
+psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-twitter-tweets-users.sql
