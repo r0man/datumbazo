4fa8d0f301eb1859d898f40dd8b0a93eb3cc039d
Roman Scherer <roman.scherer@burningswell.com>
2013-08-25 13:11:38 +0200

    Better meta data tests.

--- test/datumbazo/meta_test.clj
+++ test/datumbazo/meta_test.clj
@@ -8,10 +8,15 @@
 
 (database-test test-best-row-identifiers
   (let [columns (best-row-identifiers db :table :continents :scope :transaction)]
+    (is (= [:id] (map :name columns)))
     (is (not (empty? columns)))
-    (is (every? #(and (keyword? (:name %1))
-                      (keyword? (:type %1)))
-                columns))))
+    (is (every? #(keyword? (:name %1)) columns))
+    (is (every? #(keyword? (:type %1)) columns)))
+  (let [columns (best-row-identifiers db :table :tweets-users :scope :transaction)]
+    (is (= [:tweet-id :user-id] (map :name columns)))
+    (is (not (empty? columns)))
+    (is (every? #(keyword? (:name %1)) columns))
+    (is (every? #(keyword? (:type %1)) columns))))
 
 (database-test test-catalogs
   (let [catalogs (catalogs db)]
@@ -21,20 +26,18 @@
 (database-test test-columns
   (let [columns (columns db :table :continents)]
     (is (not (empty? columns)))
-    (is (every? #(and (keyword? (:schema %1))
-                      (keyword? (:table %1))
-                      (keyword? (:name %1))
-                      (keyword? (:type %1)))
-                columns))
+    (is (every? #(= :public (:schema %1)) columns))
+    (is (every? #(= :continents (:table %1)) columns))
+    (is (every? #(keyword? (:name %1)) columns))
+    (is (every? #(keyword? (:type %1)) columns))
     (is (= [:id :name :code :geometry :freebase-guid :geonames-id :created-at :updated-at]
            (map :name columns))))
   (let [columns (columns db :table :countries :name :continent-id)]
     (is (not (empty? columns)))
-    (is (every? #(and (keyword? (:schema %1))
-                      (keyword? (:table %1))
-                      (keyword? (:name %1))
-                      (keyword? (:type %1)))
-                columns))
+    (is (every? #(= :public (:schema %1)) columns))
+    (is (every? #(= :countries (:table %1)) columns))
+    (is (every? #(keyword? (:name %1)) columns))
+    (is (every? #(keyword? (:type %1)) columns))
     (is (= [:continent-id] (map :name columns)))))
 
 (database-test test-indexes
@@ -44,16 +47,25 @@
 (database-test test-unique-columns
   (let [columns (unique-columns db :table :continents)]
     (is (not (empty? columns)))
+    (is (every? #(= :public (:schema %1)) columns))
+    (is (every? #(= :continents (:table %1)) columns))
+    (is (every? #(keyword? (:name %1)) columns))
+    (is (every? #(keyword? (:type %1)) columns))
     (is (= [:id :name :code :freebase-guid :geonames-id] (map :name columns)))))
 
 (database-test test-primary-keys
   (let [columns (primary-keys db :table :continents)]
     (is (not (empty? columns)))
-    (is (every? #(and (keyword? (:table %1))
-                      (keyword? (:schema %1))
-                      (keyword? (:name %1)))
-                columns))
-    (is (= [:id] (map :name columns)))))
+    (is (every? #(= :public (:schema %1)) columns))
+    (is (every? #(= :continents (:table %1)) columns))
+    (is (every? #(keyword? (:name %1)) columns))
+    (is (= [:id] (map :name columns))))
+  (let [columns (primary-keys db :table :tweets-users)]
+    (is (not (empty? columns)))
+    (is (every? #(= :twitter (:schema %1)) columns))
+    (is (every? #(= :tweets-users (:table %1)) columns))
+    (is (every? #(keyword? (:name %1)) columns))
+    (is (= [:tweet-id :user-id] (map :name columns)))))
 
 (database-test test-schemas
   (let [schemas (schemas db)]
@@ -65,15 +77,15 @@
 (database-test test-tables
   (let [tables (tables db)]
     (is (not (empty? tables)))
-    (is (every? #(and (keyword? (:schema %1))
-                      (keyword? (:name %1))
-                      (= :table (:type %1))) tables))
+    (is (every? #(keyword? (:schema %1)) tables))
+    (is (every? #(keyword? (:name %1)) tables))
+    (is (every? #(= :table (:type %1)) tables))
     (is (subset? (set (map :name tables))
-                 (set [:continents :countries :spatial-ref-sys :changes :dependencies :events :projects :tags :tweets :users])))))
+                 (set [:continents :countries :spatial-ref-sys :changes :dependencies :events :projects :tags :tweets :users :tweets-users])))))
 
 (database-test test-views
   (let [views (views db)]
     (is (not (empty? views)))
-    (is (every? #(and (keyword? (:schema %1))
-                      (keyword? (:name %1))
-                      (= :view (:type %1))) views))))
+    (is (every? #(keyword? (:schema %1)) views))
+    (is (every? #(keyword? (:name %1)) views))
+    (is (every? #(= :view (:type %1)) views))))
