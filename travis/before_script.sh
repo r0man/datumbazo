c00b029c6adeedbad673b3b5b22d7b4715de3fff
Roman Scherer <roman.scherer@burningswell.com>
2013-11-24 22:27:24 +0100

    Load fixtures with lein2 on travis,

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -10,4 +10,4 @@ psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-
 psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-twitter-tweets-table.sql
 psql -U postgres datumbazo < test-resources/db/test-db/migrations/deploy/create-twitter-tweets-users.sql
 
-lein run "postgresql://tiger:scotch@localhost/datumbazo" test-resources/db/test-db/fixtures load
+lein2 run "postgresql://tiger:scotch@localhost/datumbazo" test-resources/db/test-db/fixtures load
