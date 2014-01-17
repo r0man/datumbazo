543fc91e06a1786d0cf6aa9fe9ace28081a605f6
Roman Scherer <roman.scherer@burningswell.com>
2014-01-17 10:58:00 +0100

    lein-release plugin: bumped version from 0.5.24 to 0.5.25-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datumbazo "0.5.24"
+(defproject datumbazo "0.5.25-SNAPSHOT"
   :description "Clojure Database Kung-Foo"
   :url "https://github.com/r0man/datumbazo"
   :min-lein-version "2.0.0"
