21287383a228613450af8e3438cf7193c7606d42
Roman Scherer <roman@burningswell.com>
2014-11-29 20:28:23 +0100

    Version 0.7.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datumbazo "0.7.0"
+(defproject datumbazo "0.7.1-SNAPSHOT"
   :description "Clojure Database Kung-Foo"
   :url "https://github.com/r0man/datumbazo"
   :min-lein-version "2.0.0"
