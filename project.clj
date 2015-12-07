0b440b5cdd86d8e6313b1182e473215a26270489
Roman Scherer <roman@burningswell.com>
2015-12-07 21:34:40 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datumbazo "0.7.32-SNAPSHOT"
+(defproject datumbazo "0.8.0-SNAPSHOT"
   :description "Clojure Database Kung-Foo"
   :url "https://github.com/r0man/datumbazo"
   :min-lein-version "2.0.0"
@@ -7,9 +7,9 @@
   :deploy-repositories [["releases" :clojars]]
   :dependencies [[clj-time "0.11.0"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
-                 [com.stuartsierra/component "0.3.0"]
+                 [com.stuartsierra/component "0.3.1"]
                  [commandline-clj "0.1.7"]
-                 [geo-clj "0.3.22"]
+                 [geo-clj "0.4.0"]
                  [inflections "0.10.0"]
                  [org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
@@ -19,7 +19,7 @@
                  [org.xerial/sqlite-jdbc "3.8.11.2"]
                  [mysql/mysql-connector-java "5.1.37"]
                  [slingshot "0.12.2"]
-                 [sqlingvo "0.7.15"]]
+                 [sqlingvo "0.8.0"]]
   :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.3"]
                                   [com.mchange/c3p0 "0.9.5"]
                                   [com.jolbox/bonecp "0.8.0.RELEASE"]
