efcbe2677eb16ee6e3ed9b9e37016394bdb96167
Roman Scherer <roman@burningswell.com>
2016-02-17 13:51:18 +0100

    Add identifiers support to backends

--- src/datumbazo/driver/core.clj
+++ src/datumbazo/driver/core.clj
@@ -1,26 +1,52 @@
 (ns datumbazo.driver.core
-  (:require [sqlingvo.core :refer [ast]]))
+  (:require [sqlingvo.core :refer [ast sql]]))
+
+(defmulti apply-transaction
+  "Apply `f` within a database transaction"
+  (fn [db f & [opts]] (:backend db)))
 
 (defmulti close-db
   "Close the connection to `db`."
   (fn [db] (:backend db)))
 
-(defmulti eval-db*
-  "Eval the `ast` against a database."
-  (fn [ast] (-> ast :db :backend)))
+(defmulti fetch
+  "Execute `sql` and return rows."
+  (fn [db sql & [opts]] (:backend db)))
 
-(defn eval-db
-  "Eval the `stmt` against a database."
-  [stmt & [opts]]
-  (eval-db* (ast stmt)))
+(defmulti execute
+  "Execute `sql` and return the number of affected rows."
+  (fn [db sql & [opts]] (:backend db)))
 
 (defmulti open-db
   "Open a connection to `db`."
   (fn [db] (:backend db)))
 
-(defmulti apply-transaction
-  "Open a connection to `db`."
-  (fn [db f & [opts]] (:backend db)))
+(defn eval-db
+  "Eval the `stmt` against a database."
+  [{:keys [db] :as ast} & [opts]]
+  (let [sql (sql ast)]
+    (case (:op ast)
+      :delete
+      (if (:returning ast)
+        (fetch db sql)
+        (execute db sql))
+      :except
+      (fetch db sql)
+      :insert
+      (if (:returning ast)
+        (fetch db sql)
+        (execute db sql))
+      :intersect
+      (fetch db sql)
+      :select
+      (fetch db sql)
+      :union
+      (fetch db sql)
+      :update
+      (if (:returning ast)
+        (fetch db sql)
+        (execute db sql))
+      (execute db sql))))
 
 (defn row-count
   "Normalize into a record, with the count of affected rows."
