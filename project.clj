79675a302d5f2a9bfa404d13ff2ac6cb49cbb0be
r0man <roman@burningswell.com>
2017-08-10 15:27:37 +0200

    Version 0.13.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datumbazo "0.13.5"
+(defproject datumbazo "0.13.6-SNAPSHOT"
   :description "Clojure Database Kung-Foo"
   :url "https://github.com/r0man/datumbazo"
   :min-lein-version "2.0.0"
