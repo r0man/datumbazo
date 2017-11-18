9242307ece94e25ae179af58f031d0840915ae18
r0man <roman@burningswell.com>
2017-11-18 11:38:42 +0100

    Lower case type names

--- src/datumbazo/types.clj
+++ src/datumbazo/types.clj
@@ -1,18 +1,21 @@
 (ns datumbazo.types
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.string :as str]))
 
 (defn- spec-name
   "Returns the `db` specific spec name for `type`."
   [db type]
-  (str "datumbazo." (-> db :scheme name) ".types/" (name type)))
+  (str "datumbazo." (-> db :scheme name) ".types/"
+       (-> type name str/lower-case)))
 
 (defn spec
   "Returns the `db` specific spec for `type`."
   [db type]
-  (try (s/spec (keyword (spec-name db type)))
-       (catch Exception e
-         (throw (ex-info "Can't resolve spec for type."
-                         {:type type})))))
+  (let [spec (keyword (spec-name db type))]
+    (try (s/spec spec)
+         (catch Exception e
+           (throw (ex-info "Can't resolve spec for type."
+                           {:type type :spec spec}))))))
 
 (defn gen
   "Returns the `db` specific generator for `type`."
